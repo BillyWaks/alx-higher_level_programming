f494b3a21b0e719e127a423f512b3e3d6553c60c
“BillyWaks” <136112711+BillyWaks@users.noreply.github.comgit config --global user.email 136112711+BillyWaks@users.noreply.github.com>
2024-01-10 18:37:33 -0800

    new_changes

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -3,9 +3,9 @@
 
 def add_integer(a, b=98):
     """Return the integer of a and b.
-    
+
     Float arguments are casted to ints before execution
-    
+
     Raises:
         TypeError: if either a or b is a non_integer and non-float
     """
